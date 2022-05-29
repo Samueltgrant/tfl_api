dfbbc4e917f3f17ba898572528294799744a5d49
Samueltgrant <samuel.grant@terrafirmasearch.co.uk>
2022-05-29 22:49:22 +0100

    updated secret keys

--- website/__init__.py
+++ website/__init__.py
@@ -6,7 +6,7 @@ import website.api_keys
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = api_keys.secret_key
+    app.config['SECRET_KEY'] = "FSDAJKLJKSFJADK"
     app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres: // byhxcaqmpaygky: 965f695180e2ef9bc0661fca243c36006c6de90b2390c7a5cccdddeaf9a505e8@ec2-34-230-153-41.compute-1.amazonaws.com:5432/d4quaupiqp3lpf'
     db = SQLAlchemy(app)
     migrate = Migrate(app, db)
